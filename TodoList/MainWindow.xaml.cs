f7b0ef19898c9d5e5ac57c8f213795df2fa5c5a8
PinkDeer <andreyzinovets@gmail.com>
2019-12-03 16:38:34 +0300

    File write setting

--- TodoList/MainWindow.xaml.cs
+++ TodoList/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TodoList.models;
+using TodoList.services;
 
 namespace TodoList
 {
@@ -22,7 +23,9 @@ namespace TodoList
     /// </summary>
     public partial class MainWindow : Window
     {
-        private BindingList<TodoModel> _todoDate;
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TodoModel> _todoDataList;
+        private FileIOService _fileIOService;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,13 +33,37 @@ namespace TodoList
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _todoDate = new BindingList<TodoModel>()
-            {
-                new TodoModel(){Text="test"},
-                new TodoModel(){Text="tes2"}
-            };
+            _fileIOService = new FileIOService(PATH);
 
-            dgTodoList.ItemsSource = _todoDate;
+            try
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+           
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveDate(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
         }
     }
 }
--- TodoList/models/TodoModel.cs
+++ TodoList/models/TodoModel.cs
@@ -1,29 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TodoList.models
 {
-    class TodoModel
+    class TodoModel: INotifyPropertyChanged
     {
         private bool _isDone;
         private string _text;
         public DateTime CreationDate { get; set; } = DateTime.Now;
 
-        public bool MyProperty
+        public bool isDone
         {
             get { return _isDone; }
-            set { _isDone = value; }
+            set
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
         }
 
         public string Text
         {
             get { return _text; }
-            set { _text = value; }
+            set {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
